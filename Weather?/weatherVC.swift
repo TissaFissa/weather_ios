10689c836a5415cb1f47d79418c86b45a59da492
TissaFissa <vanderheijden.thijs@outlook.com>
2017-07-12 16:57:31 +0200

    Created UI, and added Iboutlets for all objects except tableViewCell

---
+++ Weather?/weatherVC.swift
@@ -0,0 +1,34 @@
+//
+//  weatherVC.swift
+//  Weather?
+//
+//  Created by Thijs van der Heijden on 7/12/17.
+//  Copyright © 2017 Thijs van der Heijden. All rights reserved.
+//
+
+import UIKit
+
+class weatherVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
